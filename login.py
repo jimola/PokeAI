44b3124bfd9b1c284c4ce90c66c77fb741b1de29
Jacob Imola <jimolaaa@gmail.com>
2018-06-07 18:38:13 -0400

    Started making more robust message handling system

--- login.py
+++ login.py
@@ -4,9 +4,12 @@ import json
 import asyncio
 
 class SimpleAI:
-    def __init__(self):
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
         self.ws = websocket.WebSocket()
         self.ws.connect('ws://localhost:8000/showdown/websocket')
+        self.active_battles = dict()
     async def recv(self):
         w = self.ws.recv()
         print('RECEIVING: ' + w)
@@ -14,22 +17,46 @@ class SimpleAI:
     async def send(self, msg):
         print('SENDING: ' + msg)
         self.ws.send(msg)
-    async def login(self, username, password):
+
+    async def run(self):
+        await self.login()
+        while(True):
+            msg = await self.recv()
+            tokens = msg.split('|')
+            room = tokens[0]
+            cmd = tokens[1]
+            if(room == ''):
+                if cmd == 'updatechallenges':
+                    challenge_info = json.loads(tokens[2])
+                    battle_info = challenge_info['challengesFrom']
+                    from_user = next(iter(battle_info.keys()))
+                    if(from_user):
+                        await self.send('|/accept ' + from_user)
+                #Global command
+                pass
+            else:
+                #Battle command
+                pass
+    async def login(self):
         await self.send('|/autojoin')
-        await self.recv()
-        login_token = await self.recv()
+        msg = await self.recv()
+        while(True):
+            tk = msg.split('|')
+            if(tk[1] == 'challstr'):
+                login_token = msg[10:]
+                break
+            msg = await self.recv()
         print('LOGIN: ' + login_token)
         #Need to remove |challstr| from the beginning"
-        login_token = login_token[10:]
         r = requests.post('http://localhost.psim.us/action.php', 
                             data={'act':'login',
-                                'name':username,
-                                'pass':password,
+                                'name': self.username,
+                                'pass': self.password,
                                 'challstr':login_token})
         #For some reason there is a leading ']' that needs to be removed
         login_info = json.loads(r.content[1:])
         assertion = login_info['assertion']
-        await self.send('|/trn jimola,0,'+assertion)
+        await self.send('|/trn ' + self.username + ',0,'+assertion)
         #Seems to tell us about server history
         await self.recv()
 
@@ -47,39 +74,50 @@ class SimpleAI:
         self.ws.close()
         s.ws = None
 
-    async def challenge(self, username):
-        await self.send('|/challenge ' + username + ', gen7randombattle')
+    async def challenge(self, username, battle_type='random', team=None):
+        if(battle_type == 'random'):
+            await self.send('|/challenge ' + username + ', gen7randombattle')
+        else:
+            await self.send('|/utm\n'+team)
+            await self.send('|/challenge ' + username + ', gen7ou')
         incoming = ''
         while True:
             incoming = await self.recv()
             cmd_type = incoming.split('|')[1]
             if(cmd_type == 'b'):
                 break
-            else:
-                print('Last message ignored')
         #battle name and players
         self.battle_name = incoming.split('|')[2]
         self.cnt = 2
         await self.battle()
 
     async def battle(self):
+        battle_on = True
+        while battle_on:
+            msg = await self.recv()
+            split_msg = msg.split('|')
+            if split_msg[1] == 'request' and split_msg[2]:
+                content = json.loads(split_msg[2])
+                if 'forceSwitch' in content:
+                    await self.send(self.battle_name + '|/switch ' +
+                            str(self.cnt))
+                    self.cnt += 1
+                    continue
+                if 'wait' in content:
+                    continue
 
-        while(True):
-            msg = []
-            while True:
-                msg = await self.recv()
-                msg = msg.split('|')
-                if msg[1] == 'request' and msg[2]:
-                    content = json.loads(msg[2])
-                    if 'forceSwitch' in content:
-                        await self.send(self.battle_name + '|/switch ' +
-                                str(self.cnt))
-                        self.cnt += 1
+                await self.send(self.battle_name + '|/move 1')
+            elif split_msg[1] == '\n' and (split_msg[2] == 'move' 
+                    or split_msg[2] == 'switch'):
+                sub_commands = msg.split('\n|')
+                print('SCMD: ')
+                for scmd in sub_commands:
+                    s_cmd = scmd.split('|')
+                    cmd_type = s_cmd[0]
+                    print(cmd_type)
+                    if(cmd_type == 'win'):
+                        battle_on = False
                         break
-                    if 'wait' in content:
-                        break
-
-                    await self.send(self.battle_name + '|/move 1')
 
 try:
     f = open('../secret.txt').read()
@@ -87,8 +125,13 @@ try:
 except FileNotFoundError:
     print('File containing username and password not found')
 
+teams = []
+with open('teams.txt') as T:
+    for line in T:
+        teams.append(line[:-1])
+
 loop = asyncio.get_event_loop()
 s = SimpleAI()
-asyncio.ensure_future(s.login(username, password))
-asyncio.ensure_future(s.challenge('GucciMoney'))
+#asyncio.ensure_future(s.login(username, password))
+#asyncio.ensure_future(s.challenge('GucciMoney', 'ou', teams[0]))
 loop.run_forever()
